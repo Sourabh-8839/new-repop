c0a6b1d17574e5b8b0e4f042560508517480e9fc
Sourabh-8839 <sourabhsingh8839@gmail.com>
2023-03-28 09:09:11 +0530

    new changes in javascript file

--- day1/gitstart/dom.js
+++ day1/gitstart/dom.js
@@ -1,11 +1,42 @@
 
-const items=document.getElementsByClassName('list-group-item');
+// const items=document.getElementsByClassName('list-group-item');
 
-console.log(items[2]);
+// console.log(items[2]);
 
-items[2].style.backgroundColor="green";
+// items[2].style.backgroundColor="green";
 
 
-for(let i=0;i<items.length;i++){
-    items[i].style.fontWeight="Bold";
-}
+// for(let i=0;i<items.length;i++){
+//     items[i].style.fontWeight="Bold";
+// }
+
+// const header =document.getElementsByTagName('li');
+
+// console.log(header);
+
+
+const title =document.querySelector('.title');
+
+title.style.color='cyan';
+
+const item =document.querySelector('.list-group-item');
+
+// console.log(item);
+
+item.style.backgroundColor='rgba(183, 99, 225, 0.445)';
+item.style.fontWeight='700';
+
+
+const inputbtn =document.querySelector('input[type=submit]');
+
+// console.log(inputbtn);
+
+inputbtn.value='Send';
+
+
+const input =document.querySelector('input');
+
+console.log(input);
+input.placeholder='please Fill input field';
+
+
