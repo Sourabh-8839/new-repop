fe73cbe2887e9167f4e753e5a5c0b47a6ad58a28
Sourabh-8839 <sourabhsingh8839@gmail.com>
2023-04-02 09:48:28 +0530

    we store object in local storage

--- day1/gitstart/practice.js
+++ day1/gitstart/practice.js
@@ -1,7 +1,7 @@
 
 const form=document.querySelector("#my-form");
 
-const msg=document.querySelector('#msg');
+const msg=document.querySelector('.msg');
 
 const inputName = document.querySelector('#name');
 
@@ -9,7 +9,7 @@ const emailId=document.querySelector('#email');
 
 const ul=document.querySelector('#users');
 
-const btn=document.querySelector('#btn');
+const btn=document.querySelector('.btn');
 
 
 form.addEventListener('submit',onSubmit);
@@ -33,7 +33,14 @@ function onSubmit(e){
 
         ul.appendChild(li);
 
-        localStorage.setItem(emailId.value,inputName.value);
+        let myobj ={
+            name:inputName.value,
+            email:emailId.value,
+        }
+
+    
+        localStorage.setItem('user',JSON.stringify(myobj));
+        
         inputName.value='';
         emailId.value='';
 
@@ -50,10 +57,15 @@ btn.addEventListener('mouseout',(e)=>{
     document.querySelector('body').style.background="red";
 })
 
-const header=document.getElementById('header');
 
-console.log(header.textContent);
-console.log(header.innerText);
-console.log(header);
 
-console.log(header.typeOf);
+let myobj2={
+    name:'sourabh',
+    age:25
+}
+
+let myobj_serialized =JSON.stringify(myobj);
+
+localStorage.setItem('obj1',myobj_serialized);
+localStorage.setItem('obj2',JSON.stringify(myobj2));
+console.log(localStorage);
